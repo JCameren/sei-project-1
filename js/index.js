abb81aa7b1d1f3b015fc369377920e27cab9674c
JCameren <johnsonm.cameren@gmail.com>
2022-10-07 23:24:12 -0400

    rendered dealer/player arr to UI

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 const BLACKJACK = 21
 const SUITS = ['s', 'c', 'd', 'h']
 const RANKS = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A']
+const BODY = document.body
 
 
 
@@ -23,7 +24,8 @@ let winner
 
 
 /*<------------CACHED ELEMENTS----------->*/
-
+const dealerEl = document.getElementById('dealer')
+const playerEl = document.getElementById('player')
 
 
 
@@ -58,7 +60,7 @@ function init() {
             })
         })
     })
-    console.log(cards)
+    console.log('1.Card Array', cards)
     pullRandomCards(cards)
 }
 
@@ -70,22 +72,42 @@ function pullRandomCards(cardArr) {
     //sets loading animation
     isLoading = true
     //Gets random card and stores it in a variable
+ 
     const card1 = Math.floor(Math.random() * cardArr.length)
-    cardArr.splice(card1, 1)
+    // cardArr.splice(card1, 1)
     const card2 = Math.floor(Math.random() * cardArr.length)
-    cardArr.splice(card2, 1)
+    // cardArr.splice(card2, 1)
     const card3 = Math.floor(Math.random() * cardArr.length)
-    cardArr.splice(card3, 1)
+    // cardArr.splice(card3, 1)
     const card4 = Math.floor(Math.random() * cardArr.length)
-    cardArr.splice(card4, 1)
+    // cardArr.splice(card4, 1)
     console.log(card1, card2, card3, card4)
-    console.log(cardArr)
+    // console.log('2. New Card Array', cardArr)
 
     playerHand = [cardArr[card1], cardArr[card2]]
     dealerHand = [cardArr[card3], cardArr[card4]]
-    console.log(playerHand, dealerHand)
+
+    console.log(playerHand)
+
+    // renderDealerHand(dealerHand)
+    renderHands(playerHand, dealerHand)
 }
 
+function renderHands(playerArr, dealerArr) {
+    playerArr.forEach(card => {
+        const cardEl = document.createElement('div');
+        cardEl.innerHTML = `<div class="card card large ${card.face}"></div><div class="back"></div>`
+        // cardEl.className = `card card large card ${card.face}`
+        console.log(cardEl)
+        playerEl.append(cardEl)
+    })
+    dealerArr.forEach(card => {
+        const cardEl = document.createElement('div');
+        cardEl.innerHTML = `<div class="card card large ${card.face}"></div><div class="back"></div>`
+        console.log(cardEl)
+        dealerEl.append(cardEl)
+    })
+}
 
 
 
