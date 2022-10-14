364e0ea8b7f00020e693194b057b1f0999790cdd
JCameren <johnsonm.cameren@gmail.com>
2022-10-13 21:52:06 -0400

    prep for re-deploy

--- js/main.js
+++ js/main.js
@@ -19,7 +19,8 @@ const RANKS = [
 
 /*<------------STATE VARIABLES----------->*/
 let isGameOver; // if wager drops to zero
-let wager; //amount able to bet
+let bankRoll; //amount able to bet
+let bet; //amount actually bet
 let playerHand; //will be array holding player card objects
 let playerScore; //value sum of amount prop
 let dealerHand; // will be arr holding dealer card objects
@@ -31,8 +32,12 @@ const playerEl = document.getElementById("player");
 const resetBtn = document.getElementById("reset_btn");
 const hitBtn = document.getElementById("hit_btn");
 const standBtn = document.getElementById("stand_btn");
+const betSubmitBtn = document.getElementById("bet_submit_btn");
 const gameStatusEl = document.getElementById("game_status");
-const wagerEl = document.getElementById("wager_el");
+const bankEl = document.getElementById("bank_el");
+const bankRollEl = document.getElementById("amount_owned");
+const betInputEl = document.getElementById("bet_amount_input");
+const wageredEl = document.getElementById("amount_wagered");
 const pHandEl = document.getElementById("player_hand");
 const dHandEl = document.getElementById("dealer_hand");
 
@@ -40,10 +45,10 @@ const dHandEl = document.getElementById("dealer_hand");
 resetBtn.addEventListener("click", init);
 hitBtn.addEventListener("click", hitPlayer);
 standBtn.addEventListener("click", checkHands);
-// continueBtn.addEventListener("click", continueGame);
+betSubmitBtn.addEventListener("click", cacheBet);
 
 /*<------------GENERAL CHECKS----------->*/
-if (wager === 0) {
+if (bankRoll === 0) {
   isGameOver = true;
 }
 
@@ -51,24 +56,28 @@ if (wager === 0) {
 init();
 
 function init() {
+  //removing class and prop from submitEl
+  betSubmitBtn.classList.remove("disabled");
+  betSubmitBtn.disabled = false;
   //adds the listener back if it is removed upon initializing
   standBtn.addEventListener("click", checkHands);
   //removes disabled class if appended upon init
-  hitBtn.classList.remove("disabled");
+  hitBtn.classList.add("disabled");
+  hitBtn.disabled = true;
   //removes disabled class if appended upon init
-  resetBtn.classList.remove('pulse')
+  resetBtn.classList.remove("pulse");
   //removes disabled class if appended upon init
-  standBtn.classList.remove("disabled");
+  standBtn.classList.add("disabled");
+  standBtn.disabled = true;
   //init monies to 100
-  wager = 100;
+  bankRoll = 100;
+  bet = 0;
   playerScore = 0; //inits pScore to 0
   dealerScore = 0; //inits dScore to 0
   dealerEl.innerHTML = null; //makes div empty so that re-renders are clean with no elements repeating, very important
   playerEl.innerHTML = null; //makes div empty so that re-renders are clean with no elements repeating, very important
   pHandEl.innerText = `Player Hand: ${playerScore}`;
   dHandEl.innerText = `Dealer Hand: ?`;
-  hitBtn.style.visibility = "visible";
-  wagerEl.innerText = `Amount Remaining: $${wager}`;
   gameStatusEl.innerText = `Hit or Stand`;
   isGameOver = false;
   const cards = [];
@@ -84,15 +93,32 @@ function init() {
       });
     });
   });
-  console.log("1.Card Array", cards);
-  pullRandomCards(cards);
 
-  setTimeout(() => {
-    if (playerHand === BLACKJACK) {
-      wager *= 2.5;
-      checkHands();
-    }
-  }, 1500);
+  pullRandomCards(cards);
+  renderBet();
+  // bankRollEl.innerText = bankRoll
+}
+
+function cacheBet() {
+  bet = betInputEl.value;
+  if (!Number(bet)) return;
+  if (bet > bankRoll) return;
+  betSubmitBtn.classList.add("disabled");
+  console.log(bet);
+  betInputEl.value = "";
+  console.log(bet);
+  betSubmitBtn.disabled = true;
+  standBtn.addEventListener("click", checkHands);
+  standBtn.classList.remove("disabled");
+  standBtn.disabled = false;
+  hitBtn.classList.remove("disabled");
+  hitBtn.disabled = false;
+  renderBet();
+}
+
+function renderBet() {
+  wageredEl.innerText = bet;
+  bankRollEl.innerText = bankRoll;
 }
 
 function pullRandomCards(cardArr) {
@@ -101,7 +127,6 @@ function pullRandomCards(cardArr) {
   const card2 = Math.floor(Math.random() * cardArr.length);
   const card3 = Math.floor(Math.random() * cardArr.length);
   const card4 = Math.floor(Math.random() * cardArr.length);
-  console.log(card1, card2, card3, card4);
 
   playerHand = [cardArr[card1], cardArr[card2]]; //first 2 are put into the pHand, pHand becomes an arr
   //use the reduce method to obtain the value sum of each card obj
@@ -113,7 +138,6 @@ function pullRandomCards(cardArr) {
 
   dealerHand = [cardArr[card3], cardArr[card4]]; //last 2 are placed in pHand, also now an arr
 
-  console.log("1.playerHand", playerHand, "2.dealerHand", dealerHand);
   //The render functions are invoked using both arrs as arguments
   renderDealerHand(dealerHand);
   renderPlayerHand(playerHand);
@@ -129,7 +153,6 @@ function renderDealerHand(dealerArr) {
     // cardEl.classList.add(`card card back large ${card.face}`)
     //the card then gets inner html using the face prop from the card obj which will then match with existing css classes ready in the css file
     cardEl.innerHTML = `<div class="card card large ${card.face}"></div>`;
-    console.log(cardEl);
     dealerEl.append(cardEl); //adds this to the container, thus the DOM
     dealerEl.children[0].classList.add("hidden"); //first card will be hidden from player
   });
@@ -141,10 +164,8 @@ function renderPlayerHand(playerArr) {
     const cardEl = document.createElement("div"); //creates a div for that card obj
     //the card then gets inner html using the face prop from the card obj which will then match with existing css classes ready in the css file
     cardEl.innerHTML += `<div class="card card large ${card.face}"></div><div class="back"></div>`;
-    console.log("Amount of divs", cardEl);
     playerEl.append(cardEl); //adds this to the container, thus the DOM
   });
-  console.log(playerArr);
 }
 
 function hitPlayer() {
@@ -187,72 +208,73 @@ function checkHands() {
     return acc;
   }, 0);
 
-  console.log(playerScore);
   //reduce method to find value of cards in arr from the amount prop in card obj
   dealerScore = dealerHand.reduce((acc, card) => {
     acc += card.amount;
     return acc;
   }, 0);
   dHandEl.innerText = `Dealer Hand: ${dealerScore}`;
-  console.log(dealerScore);
+
   if (playerScore > BLACKJACK && dealerScore > BLACKJACK) {
     gameStatusEl.innerText = `You both busted.`;
   } else if (playerScore === BLACKJACK && dealerScore === BLACKJACK) {
     gameStatusEl.innerText = `It's a tie.`;
   } else if (playerScore === BLACKJACK) {
-    wager *= 2.5;
+    bankRoll += Number(bet) * 2.5;
     gameStatusEl.innerText = `You got blackjack!`;
   } else if (playerScore > BLACKJACK) {
     gameStatusEl.innerText = `You busted. Dealer wins!`;
-    wager -= 50;
+    bankRoll -= Number(bet);
   } else if (dealerScore > BLACKJACK) {
     gameStatusEl.innerText = `The dealer busted! You win!`;
-    wager += 50;
+    bankRoll += Number(bet);
   } else if (playerScore > dealerScore) {
     gameStatusEl.innerText = `You win!`;
-    wager += 50;
+    bankRoll += Number(bet);
   } else if (playerScore < dealerScore) {
     gameStatusEl.innerText = `Dealer wins!`;
-    wager -= 50;
+    bankRoll -= Number(bet);
   } else if (playerScore === dealerScore) {
     gameStatusEl.innerText = `It's a tie.`;
   }
 
-  wagerEl.innerHTML = `Amount Remaining: $${wager}`;
-  if (wager <= 0) {
+  if (bankRoll <= 0) {
     wager = 0;
-    wagerEl.innerText = `Amount Remaining: $${wager}`;
     gameStatusEl.innerText = `Game Over.`;
-    // continueBtn.classList.add("disabled");
     standBtn.classList.add("disabled");
   }
-  console.log(isGameOver);
+
   standBtn.removeEventListener("click", checkHands);
   setTimeout(() => {
     continueGame();
   }, 2000);
+  renderBet();
 }
 
 function continueGame() {
-  if (wager <= 0) {
-    wager = 0;
+  if (bankRoll <= 0) {
+    bankRoll = 0;
+    bet = 0;
     gameStatusEl.innerText = `Game Over.`;
-    wagerEl.innerText = `Amount Remaining: $${wager}`;
-    resetBtn.classList.add('pulse')
+    resetBtn.classList.add("pulse");
     isGameOver = true;
+    renderBet();
     return;
   } //wont work if the player has no money
-  standBtn.addEventListener("click", checkHands);
-  standBtn.classList.remove("disabled");
-  hitBtn.classList.remove("disabled");
+  betSubmitBtn.classList.remove("disabled");
+  betSubmitBtn.disabled = false;
+  standBtn.classList.add("disabled");
+  standBtn.disabled = true;
+  hitBtn.classList.add("disabled");
+  hitBtn.disabled = true;
   playerScore = 0;
   dealerScore = 0;
+  bet = 0;
   dealerEl.innerHTML = null;
   playerEl.innerHTML = null;
   pHandEl.innerText = `Player Hand: ${playerScore}`;
   dHandEl.innerText = `Dealer Hand: ?`;
   hitBtn.style.visibility = "visible";
-  wagerEl.innerText = `Amount Remaining: $${wager}`;
   gameStatusEl.innerText = `Hit or Stand`;
   isGameOver = false;
   const cards = [];
@@ -268,6 +290,7 @@ function continueGame() {
       });
     });
   });
-  console.log("1.Card Array", cards);
+
   pullRandomCards(cards);
+  renderBet();
 }
